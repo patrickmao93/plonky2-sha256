30a3e597daaa5d748f1d4851b3a73f46a54adb61
Unknown <1650091775@qq.com>
2023-02-03 10:41:41 +0800

    change recursive implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![feature(generic_const_exprs)]
+
 use std::io::Write;
 use std::fs::File;
 use std::path::Path;
@@ -39,7 +41,6 @@ pub fn prove_sha256(msg: &[u8]) -> Result<()> {
     type C = PoseidonGoldilocksConfig;
     type F = <C as GenericConfig<D>>::F;
     let config = CircuitConfig::standard_recursion_config();
-    // config.fri_config.rate_bits = 3;
     println!("{:?}", config);
 
     let mut builder = CircuitBuilder::<F, D>::new(config);
@@ -76,7 +77,8 @@ pub fn prove_sha256(msg: &[u8]) -> Result<()> {
     println!("proof steps length {}", sha256proof.proof.opening_proof.query_round_proofs[0].steps.len());
 
     let config = CircuitConfig::standard_recursion_config();
-    let proof = recursive_proof::<F, C, C, D>(&(sha256proof, data.verifier_only, data.common), None, &config, None)?;
+    // let proof = recursive_proof::<F, C, C, D>(&(sha256proof, data.verifier_only, data.common), None, &config, None)?;
+    let proof =recursive_proof::<F, C, C, D>(sha256proof, data.verifier_only, data.common, &config, None, true, true)?;
 
     println!("middle proof steps {:?}", proof.0.proof.opening_proof.query_round_proofs[0].steps.len());
 
@@ -100,11 +102,6 @@ pub fn prove_sha256(msg: &[u8]) -> Result<()> {
     let mut conf_file = File::create("./circom/test/data/conf.json")?;
     conf_file.write_all(serde_json::to_string(&conf)?.as_ref())?;
 
-    // let timing = TimingTree::new("verify", Level::Debug);
-    // let res = data.verify(proof);
-    // timing.print();
-
-    // res
     Ok(())
 }
 
@@ -129,62 +126,51 @@ fn recursive_proof<
     InnerC: GenericConfig<D, F = F>,
     const D: usize,
 >(
-    inner1: &ProofTuple<F, InnerC, D>,
-    inner2: Option<ProofTuple<F, InnerC, D>>,
+    inner_proof: ProofWithPublicInputs<F, InnerC, D>,
+    inner_vd: VerifierOnlyCircuitData<InnerC, D>,
+    inner_cd: CommonCircuitData<F, D>,
     config: &CircuitConfig,
     min_degree_bits: Option<usize>,
-) -> Result<ProofTuple<F, C, D>>
+    print_gate_counts: bool,
+    print_timing: bool,
+) -> Result<(
+    ProofWithPublicInputs<F, C, D>,
+    VerifierOnlyCircuitData<C, D>,
+    CommonCircuitData<F, D>,
+)>
 where
     InnerC::Hasher: AlgebraicHasher<F>,
-    // [(); C::Hasher::HASH_SIZE]:,
+    [(); C::Hasher::HASH_SIZE]:,
 {
     let mut builder = CircuitBuilder::<F, D>::new(config.clone());
     let mut pw = PartialWitness::new();
+    let pt = builder.add_virtual_proof_with_pis::<InnerC>(&inner_cd);
+    pw.set_proof_with_pis_target(&pt, &inner_proof);
 
-    {
-        let (inner_proof, inner_vd, inner_cd) = inner1;
-        let pt = builder.add_virtual_proof_with_pis::<InnerC>(inner_cd);
-        pw.set_proof_with_pis_target(&pt, inner_proof);
-        builder.register_public_inputs(&*pt.public_inputs);
+    let inner_data = VerifierCircuitTarget {
+        constants_sigmas_cap: builder.add_virtual_cap(inner_cd.config.fri_config.cap_height),
+        circuit_digest: builder.add_virtual_hash(),
+    };
+    pw.set_cap_target(
+        &inner_data.constants_sigmas_cap,
+        &inner_vd.constants_sigmas_cap,
+    );
+    pw.set_hash_target(inner_data.circuit_digest, inner_vd.circuit_digest);
 
-        let inner_data = VerifierCircuitTarget {
-            constants_sigmas_cap: builder.add_virtual_cap(inner_cd.config.fri_config.cap_height),
-            circuit_digest: builder.add_virtual_hash(),
-        };
-        pw.set_cap_target(
-            &inner_data.constants_sigmas_cap,
-            &inner_vd.constants_sigmas_cap,
-        );
-        pw.set_hash_target(inner_data.circuit_digest, inner_vd.circuit_digest);
-
-        builder.verify_proof::<InnerC>(&pt, &inner_data, inner_cd);
+    builder.register_public_inputs(inner_data.circuit_digest.elements.as_slice());
+    for i in 0..builder.config.fri_config.num_cap_elements() {
+        builder.register_public_inputs(&inner_data.constants_sigmas_cap.0[i].elements);
     }
+    builder.verify_proof::<InnerC>(&pt, &inner_data, &inner_cd);
 
-    if inner2.is_some() {
-        let (inner_proof, inner_vd, inner_cd) = inner2.unwrap();
-        let pt = builder.add_virtual_proof_with_pis::<InnerC>(&inner_cd);
-        pw.set_proof_with_pis_target(&pt, &inner_proof);
-        builder.register_public_inputs(&*pt.public_inputs);
-
-        let inner_data = VerifierCircuitTarget {
-            constants_sigmas_cap: builder.add_virtual_cap(inner_cd.config.fri_config.cap_height),
-            circuit_digest: builder.add_virtual_hash(),
-        };
-        pw.set_hash_target(inner_data.circuit_digest, inner_vd.circuit_digest);
-        pw.set_cap_target(
-            &inner_data.constants_sigmas_cap,
-            &inner_vd.constants_sigmas_cap,
-        );
-
-        builder.verify_proof::<InnerC>(&pt, &inner_data, &inner_cd);
+    if print_gate_counts {
+        builder.print_gate_counts(0);
     }
-    builder.print_gate_counts(0);
 
     if let Some(min_degree_bits) = min_degree_bits {
-        // We don't want to pad all the way up to 2^min_degree_bits, as the builder will
-        // add a few special gates afterward. So just pad to 2^(min_degree_bits
-        // - 1) + 1. Then the builder will pad to the next power of two,
-        // 2^min_degree_bits.
+        // We don't want to pad all the way up to 2^min_degree_bits, as the builder will add a
+        // few special gates afterward. So just pad to 2^(min_degree_bits - 1) + 1. Then the
+        // builder will pad to the next power of two, 2^min_degree_bits.
         let min_gates = (1 << (min_degree_bits - 1)) + 1;
         for _ in builder.num_gates()..min_gates {
             builder.add_gate(NoopGate, vec![]);
@@ -193,12 +179,94 @@ where
 
     let data = builder.build::<C>();
 
-    let mut timing = TimingTree::new("prove", Level::Info);
+    let mut timing = TimingTree::new("prove", Level::Debug);
     let proof = prove(&data.prover_only, &data.common, pw, &mut timing)?;
-    timing.print();
+    if print_timing {
+        timing.print();
+    }
 
+    println!("######################### recursive verify #########################");
     data.verify(proof.clone())?;
 
-    // test_serialization(&proof, &data.verifier_only, &data.common)?;
     Ok((proof, data.verifier_only, data.common))
 }
+
+// fn recursive_proof<
+//     F: RichField + Extendable<D>,
+//     C: GenericConfig<D, F = F>,
+//     InnerC: GenericConfig<D, F = F>,
+//     const D: usize,
+// >(
+//     inner1: &ProofTuple<F, InnerC, D>,
+//     inner2: Option<ProofTuple<F, InnerC, D>>,
+//     config: &CircuitConfig,
+//     min_degree_bits: Option<usize>,
+// ) -> Result<ProofTuple<F, C, D>>
+// where
+//     InnerC::Hasher: AlgebraicHasher<F>,
+//     [(); C::Hasher::HASH_SIZE]:,
+// {
+//     let mut builder = CircuitBuilder::<F, D>::new(config.clone());
+//     let mut pw = PartialWitness::new();
+
+//     {
+//         let (inner_proof, inner_vd, inner_cd) = inner1;
+//         let pt = builder.add_virtual_proof_with_pis::<InnerC>(inner_cd);
+//         pw.set_proof_with_pis_target(&pt, inner_proof);
+//         builder.register_public_inputs(&*pt.public_inputs);
+
+//         let inner_data = VerifierCircuitTarget {
+//             constants_sigmas_cap: builder.add_virtual_cap(inner_cd.config.fri_config.cap_height),
+//             circuit_digest: builder.add_virtual_hash(),
+//         };
+//         pw.set_cap_target(
+//             &inner_data.constants_sigmas_cap,
+//             &inner_vd.constants_sigmas_cap,
+//         );
+//         pw.set_hash_target(inner_data.circuit_digest, inner_vd.circuit_digest);
+
+//         builder.verify_proof::<InnerC>(&pt, &inner_data, inner_cd);
+//     }
+
+//     if inner2.is_some() {
+//         let (inner_proof, inner_vd, inner_cd) = inner2.unwrap();
+//         let pt = builder.add_virtual_proof_with_pis::<InnerC>(&inner_cd);
+//         pw.set_proof_with_pis_target(&pt, &inner_proof);
+//         builder.register_public_inputs(&*pt.public_inputs);
+
+//         let inner_data = VerifierCircuitTarget {
+//             constants_sigmas_cap: builder.add_virtual_cap(inner_cd.config.fri_config.cap_height),
+//             circuit_digest: builder.add_virtual_hash(),
+//         };
+//         pw.set_hash_target(inner_data.circuit_digest, inner_vd.circuit_digest);
+//         pw.set_cap_target(
+//             &inner_data.constants_sigmas_cap,
+//             &inner_vd.constants_sigmas_cap,
+//         );
+
+//         builder.verify_proof::<InnerC>(&pt, &inner_data, &inner_cd);
+//     }
+//     builder.print_gate_counts(0);
+
+//     if let Some(min_degree_bits) = min_degree_bits {
+//         // We don't want to pad all the way up to 2^min_degree_bits, as the builder will
+//         // add a few special gates afterward. So just pad to 2^(min_degree_bits
+//         // - 1) + 1. Then the builder will pad to the next power of two,
+//         // 2^min_degree_bits.
+//         let min_gates = (1 << (min_degree_bits - 1)) + 1;
+//         for _ in builder.num_gates()..min_gates {
+//             builder.add_gate(NoopGate, vec![]);
+//         }
+//     }
+
+//     let data = builder.build::<C>();
+
+//     let mut timing = TimingTree::new("prove", Level::Info);
+//     let proof = prove(&data.prover_only, &data.common, pw, &mut timing)?;
+//     timing.print();
+
+//     data.verify(proof.clone())?;
+
+//     // test_serialization(&proof, &data.verifier_only, &data.common)?;
+//     Ok((proof, data.verifier_only, data.common))
+// }
